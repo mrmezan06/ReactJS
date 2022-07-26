33aba6b12b30b9bc7e861c9fced74845da4d76ea
Mejanur Rahman Mezan <43064536+mrmezan06@users.noreply.github.com>
2022-07-27 04:05:21 +0600

    Initial commit

---
+++ hello-world/src/components/Greet.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+// Functional Component Example
+
+// Javascript function that returns a React component
+/* function Greet() {
+  return <h1>Hello Akash</h1>;
+}
+
+ */
+// ES6 style React component
+/* const Greet = () => <h1>Hello Akash</h1>;
+ */
+
+const Greet = () => <h1>Hello Akash</h1>
+
+export default Greet;
+
+
+// Name Export doesn't work in ES6 style React component why I don't know.
+
---
+++ hello-world/src/components/Welcome.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+// Class Component Example
+
+class Welcome extends Component {
+  render() {
+    return <h1>Welcome Akash</h1>;
+  }
+}
+
+export default Welcome;
